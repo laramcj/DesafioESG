781b3d76294478454479877c6f0b1af7c6fc5122
laramcj <lara.mcj@gmail.com>
2022-06-12 20:56:34 -0300

    inicio do projeto

---
+++ hortalize/src/app/pages/add-plants/add-plants.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-plants',
+  templateUrl: './add-plants.component.html',
+  styleUrls: ['./add-plants.component.css']
+})
+export class AddPlantsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
