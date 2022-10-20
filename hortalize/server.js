8e6b2a4b414f4347991c6fa755aad94303c69e68
laramcj <lara.mcj@gmail.com>
2022-10-20 09:31:53 -0300

    Subir para o heroku

--- hortalize/server.js
+++ hortalize/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/hortalize'));
+app.use(express.static('dist/simulador_financeiro'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/hortalize/'}),
+    res.sendFile('index.html', {root: 'dist/simulador_financeiro/'}),
 );
 
 // Start the app by listening on the default Heroku port
